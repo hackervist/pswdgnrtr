464c64c533154acda43f4ddffc72b3899253b37c
h <hackervist@yahoo.com>
2020-11-03 18:40:56 -0600

    nothing new

--- psdgnrtr.py
+++ psdgnrtr.py
@@ -1,16 +1,33 @@
-import string
-import random
+import string, random
+# import 
 
 
-def password(length, num=False, strength='weak'):
+def getRandomNums():
+    pass
+
+def getRandomPunc():
+    pass
+
+def getRandomLetters():
+    pass
+
+
+def password(length =8, num=False, strength='weak'):
     """Length of password, num if you want a number, 
     and strenth (weak. strong, very strong"""
+    
+    # setup charaters
     lower = string.ascii_lowercase
     upper = string.ascii_uppercase
     letter = lower + upper
     dig = string.digits
     punc = string.punctuation
     pwd = ''
+    
+    
+    if length < 8:
+        length = 8
+    
     if strength == 'weak':
         if num:
             length -= 2
@@ -27,19 +44,21 @@ def password(length, num=False, strength='weak'):
 
         for i in range(length):
             pwd += random.choice(letter)
-    elif strength == 'very':
-        ranNums = random.randint(2, 4) # random amount of numbers 
+    elif strength == 'paranoid':
+        ranNums = random.randint(3, 6) # random amount of numbers 
         ranPunc = random.randint(2, 4) # random amount of punctuations 
         # print (ranNums)
+        length *=2
+        if length > 30:
+            length = 30
         if num:
-            length -= ranNums
+            length += ranNums
             for i in range(ranNums):
                 pwd += random.choice(dig)
             length -= ranPunc 
             for i in range(ranPunc ):
                 pwd += random.choice(punc)
-
-        for i in range(length):
+        for i in range(length ):
             pwd += random.choice(letter)
 
     pwd = list(pwd)
@@ -47,9 +66,9 @@ def password(length, num=False, strength='weak'):
 
     return ''.join(pwd)
 
-# minimum should be 6
+# minimum should be 8
 print(password(6, True, 'weak'))
 
 print(password(6, True, 'strong'))
 
-print(password(6, False, 'very'))
+print(password(26, True, 'paranoid'))
